89d097a3825e9e95ad2dc16624f1b0b08a54981c
joaomiguelpassos <atmkupp3r@gmail.com>
2020-10-27 15:33:33 +0000

    Fresh Start

--- app/src/main/java/com/example/pm_22689/MainActivity.kt
+++ app/src/main/java/com/example/pm_22689/MainActivity.kt
@@ -1,20 +1,19 @@
 package com.example.pm_22689
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
-import android.view.View
 import android.widget.Toast
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import com.example.pm_22689.adapters.NotesAdapter
 
 const val PARAM1_NAME = "PARAM1_NAME"
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -33,13 +32,11 @@ class MainActivity : AppCompatActivity() {
                 true
             }
             R.id.view_notes -> {
-                Toast.makeText(this, "ver notas", Toast.LENGTH_SHORT).show()
                 val intent = Intent(this, NotesActivity::class.java)
-                startActivity(intent)
+                startActivity(intent)           //Abre a atividade de consulta de notas
                 true
             }
             else -> super.onOptionsItemSelected(item)               //returns false
         }
     }
 }
-
--- app/src/main/java/com/example/pm_22689/NotesActivity.kt
+++ app/src/main/java/com/example/pm_22689/NotesActivity.kt
@@ -1,20 +1,23 @@
 package com.example.pm_22689
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.pm_22689.adapters.NotesAdapter
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class NotesActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_notes)
 
-        //recycler view
-        val recyclerView = findViewById<RecyclerView>(R.id.recyclerview)
-        val adapter = NotesAdapter(this)
-        recyclerView.adapter = adapter
-        recyclerView.layoutManager = LinearLayoutManager(this)
     }
+
 }
