88b28a3dbd69ad02a97f395755f508879b74379f
johnsteps <joaomiguelpassos@outlook.pt>
2020-11-30 16:13:00 +0000

    Points of interest working, but need improvement

--- app/src/main/java/com/example/pm_22689/MapsActivity.kt
+++ app/src/main/java/com/example/pm_22689/MapsActivity.kt
@@ -63,6 +63,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         map.addMarker(MarkerOptions().position(homeLatLng))
 
         setMapLongClick(map)
+        setPoiClick(map)
     }
 
     private fun setMapLongClick(map: GoogleMap) {
@@ -79,6 +80,17 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 .title(getString(R.string.dropped_pin))
                 .snippet(snippet)
             )
+        }
+    }
+
+    private fun setPoiClick(map: GoogleMap) {
+        map.setOnPoiClickListener { poi ->
+            val poiMarker = map.addMarker(
+                MarkerOptions()
+                    .position(poi.latLng)
+                    .title(poi.name)
+            )
+            poiMarker.showInfoWindow()
         }
     }
 
